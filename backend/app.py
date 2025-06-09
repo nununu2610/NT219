7a0e0b92de665e3f3351eaaafe7bbded1feb88d3
nununu2610 <nnhu7732@gmail.com>
2025-06-09 16:17:57 +0700

    n

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from flask import Flask, g, jsonify
 from flask_cors import CORS
 from db import init_db, get_db, close_db
